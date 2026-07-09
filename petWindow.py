be2a5decde51874524cda2df20cddf74a38097ef
Jacob Chisholm <jchish9900@gmail.com>
2026-07-09 18:35:08 -0400

    3 functions added for pet state and screen size

--- petWindow.py
+++ petWindow.py
@@ -22,3 +22,45 @@ class PetWindow(QWidget):
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.resize(200, 200)
         self.show()
+
+        self.dragging = False
+        self.drag_start = None
+
+        self.favorite_spot = None
+
+        screen = QApplication.primaryScreen()
+        geometry = screen.geometry()
+
+        screen_width = geometry.width()
+        screen_height = geometry.height()
+
+        pet_width = 200
+        pet_height = 200
+        taskbar_gap = 50
+
+        x = screen_width - pet_width
+        y = screen_height - pet_height - taskbar_gap
+
+        self.move(x, y)
+
+        print("Screen:", screen_width, screen_height)
+        print("Starting pet at:", x, y)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = True
+            self.drag_start = event.globalPosition()
+    
+    def mouseMoveEvent(self, event):
+        if self.dragging:
+            current_position = event.globalPosition()
+            delta = current_position - self.drag_start
+
+            self.move(self.pos() + delta.toPoint())
+
+            self.drag_start = current_position
+    
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = False
+            self.drag_start = None
